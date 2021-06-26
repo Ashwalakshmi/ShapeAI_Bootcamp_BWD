59ba97e02432d88c07fc8c33297897534f40fd34
Ashwalakshmi <86288359+Ashwalakshmi@users.noreply.github.com>
2021-06-26 19:48:33 +0530

    Add files via upload
    java script

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Project</h1>
+      <p>Name: G.Ashwalakshmi<br/>course: Java script<br/>Project</p>
+    </div>
+  );
+}
+
+export default Info;
